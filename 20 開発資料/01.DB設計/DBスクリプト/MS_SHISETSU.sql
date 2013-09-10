218d4a0d8fa650cbe6646bd3f594b0d3566b0d76
Kijitani <kijitani@localhost>
2013-09-10 11:55:03 +0000

    Vss2Git

---
+++ 20 開発資料/01.DB設計/DBスクリプト/MS_SHISETSU.sql
@@ -0,0 +1,30 @@
+USE [BAYER_DEMO]
+GO
+/****** オブジェクト:  Table [dbo].[MS_SHISETSU]    ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[MS_SHISETSU](
+	[SYSTEM_ID] [nvarchar](10) COLLATE Japanese_CI_AS  NOT NULL,
+	[ADDRESS1] [nvarchar](200) COLLATE Japanese_CI_AS  NULL,
+	[ADDRESS2] [nvarchar](200) COLLATE Japanese_CI_AS  NULL,
+	[SHISETSU_NAME] [nvarchar](200) COLLATE Japanese_CI_AS  NULL,
+	[TEL] [nvarchar](20) COLLATE Japanese_CI_AS  NULL,
+	[CHECKIN_TIME] [nvarchar](20) COLLATE Japanese_CI_AS  NULL,
+	[CHECKOUT_TIME] [nvarchar](20) COLLATE Japanese_CI_AS  NULL,
+	[STOP_FLG] [nvarchar](1) COLLATE Japanese_CI_AS  NULL,
+	[INPUT_DATE] [nvarchar](20) COLLATE Japanese_CI_AS  NULL,
+	[INPUT_USER] [nvarchar](10) COLLATE Japanese_CI_AS  NULL,
+	[UPDATE_DATE] [nvarchar](20) COLLATE Japanese_CI_AS  NULL,
+	[UPDATE_USER] [nvarchar](10) COLLATE Japanese_CI_AS  NULL,
+ CONSTRAINT [PK_MS_SHISETSU] PRIMARY KEY CLUSTERED 
+(
+	[SYSTEM_ID] ASC
+) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+SET ANSI_PADDING OFF
---
+++ 20 開発資料/01.DB設計/DBスクリプト/MS_USER.sql
@@ -0,0 +1,28 @@
+USE [BAYER_DEMO]
+GO
+/****** オブジェクト:  Table [dbo].[MS_USER]    ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[MS_USER](
+	[SYSTEM_ID] [nvarchar](10) COLLATE Japanese_CI_AS NULL,
+	[LOGIN_ID] [nvarchar](20) COLLATE Japanese_CI_AS NOT NULL,
+	[PASSWORD] [nvarchar](20) COLLATE Japanese_CI_AS NULL,
+	[KENGEN] [nvarchar](1) COLLATE Japanese_CI_AS NULL,
+	[USER_NAME] [nvarchar](100) COLLATE Japanese_CI_AS NULL,
+	[STOP_FLG] [nvarchar](1) COLLATE Japanese_CI_AS NULL,
+	[INPUT_DATE] [nvarchar](20) COLLATE Japanese_CI_AS NULL,
+	[INPUT_USER] [nvarchar](20) COLLATE Japanese_CI_AS NULL,
+	[UPDATE_DATE] [nvarchar](20) COLLATE Japanese_CI_AS NULL,
+	[UPDATE_USER] [nvarchar](20) COLLATE Japanese_CI_AS NULL,
+ CONSTRAINT [PK_MS_USER] PRIMARY KEY CLUSTERED 
+(
+	[LOGIN_ID] ASC
+) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+SET ANSI_PADDING OFF
