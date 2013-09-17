686f55f5ffd0df1d5c7793a43d7063c8dcb3458c
Kijitani <kijitani@localhost>
2013-09-17 06:54:16 +0000

    Vss2Git

--- 50 開発/Bayer.root/Bayer/Bayer/DrReport.Designer.vb
+++ 50 開発/Bayer.root/Bayer/Bayer/DrReport.Designer.vb
@@ -21,35 +21,70 @@ Partial Public Class DrReport
         Me.PageHeader = New DataDynamics.ActiveReports.PageHeader
         Me.Detail = New DataDynamics.ActiveReports.Detail
         Me.PageFooter = New DataDynamics.ActiveReports.PageFooter
+        Me.Label1 = New DataDynamics.ActiveReports.Label
+        Me.Label2 = New DataDynamics.ActiveReports.Label
+        CType(Me.Label1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.Label2, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me, System.ComponentModel.ISupportInitialize).BeginInit()
         '
         'PageHeader
         '
-        Me.PageHeader.Height = 0.25F
+        Me.PageHeader.Controls.AddRange(New DataDynamics.ActiveReports.ARControl() {Me.Label1, Me.Label2})
+        Me.PageHeader.Height = 0.7291666!
         Me.PageHeader.Name = "PageHeader"
         '
         'Detail
         '
-        Me.Detail.Height = 2.0F
+        Me.Detail.ColumnSpacing = 0.0!
+        Me.Detail.Height = 2.0!
         Me.Detail.Name = "Detail"
         '
         'PageFooter
         '
-        Me.PageFooter.Height = 0.25F
+        Me.PageFooter.Height = 0.25!
         Me.PageFooter.Name = "PageFooter"
+        '
+        'Label1
+        '
+        Me.Label1.Height = 0.3125!
+        Me.Label1.HyperLink = Nothing
+        Me.Label1.Left = 1.958333!
+        Me.Label1.Name = "Label1"
+        Me.Label1.Style = ""
+        Me.Label1.Text = "Label1"
+        Me.Label1.Top = 0.1354167!
+        Me.Label1.Width = 3.197917!
+        '
+        'Label2
+        '
+        Me.Label2.Height = 0.21875!
+        Me.Label2.HyperLink = Nothing
+        Me.Label2.Left = 5.218898!
+        Me.Label2.Name = "Label2"
+        Me.Label2.Style = ""
+        Me.Label2.Text = "1/2"
+        Me.Label2.Top = 0.4480315!
+        Me.Label2.Width = 0.78125!
         '
         'DrReport
         '
+        Me.MasterReport = False
         Me.PageSettings.PaperHeight = 11.0!
         Me.PageSettings.PaperWidth = 8.5!
-        Me.PrintWidth = 6.0!
+        Me.PrintWidth = 6.000148!
         Me.Sections.Add(Me.PageHeader)
         Me.Sections.Add(Me.Detail)
         Me.Sections.Add(Me.PageFooter)
-        Me.StyleSheet.Add(New DDCssLib.StyleSheetRule("font-style: normal; text-decoration: none; font-weight: normal; font-size: 10pt; color: Black; font-family: MS UI Gothic; ddo-char-set: 128; ", "Normal"))
-        Me.StyleSheet.Add(New DDCssLib.StyleSheetRule("font-size: 16pt; font-weight: bold; ", "Heading1", "Normal"))
-        Me.StyleSheet.Add(New DDCssLib.StyleSheetRule("font-size: 14pt; font-weight: bold; ", "Heading2", "Normal"))
-        Me.StyleSheet.Add(New DDCssLib.StyleSheetRule("font-size: 13pt; font-weight: bold; ", "Heading3", "Normal"))
+        Me.StyleSheet.Add(New DDCssLib.StyleSheetRule("font-style: normal; text-decoration: none; font-weight: normal; font-size: 10pt; " & _
+                    "color: Black; font-family: MS UI Gothic; ddo-char-set: 128", "Normal"))
+        Me.StyleSheet.Add(New DDCssLib.StyleSheetRule("font-size: 16pt; font-weight: bold", "Heading1", "Normal"))
+        Me.StyleSheet.Add(New DDCssLib.StyleSheetRule("font-size: 14pt; font-weight: bold", "Heading2", "Normal"))
+        Me.StyleSheet.Add(New DDCssLib.StyleSheetRule("font-size: 13pt; font-weight: bold", "Heading3", "Normal"))
+        CType(Me.Label1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.Label2, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me, System.ComponentModel.ISupportInitialize).EndInit()
+
     End Sub
+    Private WithEvents Label1 As DataDynamics.ActiveReports.Label
+    Private WithEvents Label2 As DataDynamics.ActiveReports.Label
 End Class 
