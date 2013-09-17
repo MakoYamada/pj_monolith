9148cc6cf46f9b7e2bd983fbadf55b748ec61a92
Tanaka <tanaka@localhost>
2013-09-17 08:41:17 +0000

    Vss2Git

--- 50 開発/Bayer.root/Bayer/Bayer/Common/URL.vb
+++ 50 開発/Bayer.root/Bayer/Bayer/Common/URL.vb
@@ -18,5 +18,6 @@ Public Class URL
     Public Shared Preview As String = WebConfig.Site.URL & "Preview.aspx"
     Public Shared DrList As String = WebConfig.Site.URL & "DrList.aspx"
     Public Shared DrRegist As String = WebConfig.Site.URL & "DrRegist.aspx"
+    Public Shared HotelKensaku As String = WebConfig.Site.URL & "HotelKensaku.aspx"
 
 End Class
