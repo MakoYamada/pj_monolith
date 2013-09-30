aad007e3e2e2b2ef31ddb379fcd5e9f9740e754b
Kijitani <kijitani@localhost>
2013-09-30 01:33:14 +0000

    Vss2Git

---
+++ 50 開発/Bayer.root/Bayer/ImportKouenkai/Proc.vb
@@ -0,0 +1,15 @@
+﻿Imports CommonLib
+
+Public Class Proc
+    Inherits batchBase
+
+    Private Const pbatchID As String = "ImportKouenkai" 'バッチID
+
+    Public Sub New()
+        MyBase.New(pbatchID)
+    End Sub
+
+    Public Overrides Sub run()
+
+    End Sub
+End Class
