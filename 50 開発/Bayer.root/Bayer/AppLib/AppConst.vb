4699ca294d58f92775031e6065679e46098dc251
Tanaka <tanaka@localhost>
2013-09-30 05:36:27 +0000

    Vss2Git

--- 50 開発/Bayer.root/Bayer/AppLib/AppConst.vb
+++ 50 開発/Bayer.root/Bayer/AppLib/AppConst.vb
@@ -315,6 +315,15 @@ Public Class AppConst
         Public Const ROOM_TYPE As String = "11"             'èhîëïîâÆÉ^ÉCÉv
     End Class
 
+    Public Class STOP_FLG
+        Public Class Code
+            Public Const [Stop] As String = "1"
+        End Class
+        Public Class Name
+            Public Const [Stop] As String = "óòópí‚é~"
+        End Class
+    End Class
+
     Public Class MS_USER
         Public Class KENGEN
             '????
@@ -325,14 +334,6 @@ Public Class AppConst
             Public Class Name
                 Public Const KENGEN_1 As String = "å†å¿ÇªÇÃÇP"
                 Public Const KENGEN_2 As String = "å†å¿ÇªÇÃÇQ"
-            End Class
-        End Class
-        Public Class STOP_FLG
-            Public Class Code
-                Public Const [Stop] As String = "1"
-            End Class
-            Public Class Name
-                Public Const [Stop] As String = "óòópí‚é~"
             End Class
         End Class
     End Class
