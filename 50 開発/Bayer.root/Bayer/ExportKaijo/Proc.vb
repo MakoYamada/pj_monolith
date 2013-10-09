87e38bb7ebcc1a6440827de592b9650b5b3a6e31
Kijitani <kijitani@localhost>
2013-10-09 08:02:56 +0000

    Vss2Git

---
+++ 50 開発/Bayer.root/Bayer/ExportKaijo/Proc.vb
@@ -0,0 +1,16 @@
+﻿Imports AppLib
+Imports CommonLib
+Imports System.IO
+Public Class Proc
+    Inherits batchBase
+
+    Private Const pbatchID As String = "ExportKaijo" 'バッチID
+
+    Public Sub New()
+        MyBase.New(pbatchID)
+    End Sub
+
+    Public Overrides Sub run()
+
+    End Sub
+End Class
