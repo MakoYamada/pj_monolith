afdbe86ba7cbecb97400b2cde827aeda527ea1ba
Kijitani <kijitani@localhost>
2013-10-15 06:22:33 +0000

    Vss2Git

---
+++ 50 開発/Bayer.root/Bayer/ImportSeisan/Proc.vb
@@ -0,0 +1,17 @@
+﻿Imports AppLib
+Imports CommonLib
+Imports System.IO
+Public Class Proc
+    Inherits batchBase
+
+    Private Const pbatchID As String = "ImportSanka" 'バッチID
+    Private Const pDelimiter As String = ","
+
+    Public Sub New()
+        MyBase.New(pbatchID)
+    End Sub
+
+    Public Overrides Sub run()
+
+    End Sub
+End Class
