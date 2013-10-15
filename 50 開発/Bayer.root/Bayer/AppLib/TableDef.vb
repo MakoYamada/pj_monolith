10e9e0b6959e1ef793c3ca0e94338c31bc64487d
Tanaka <tanaka@localhost>
2013-10-15 03:58:04 +0000

    Vss2Git

--- 50 開発/Bayer.root/Bayer/AppLib/TableDef.vb
+++ 50 開発/Bayer.root/Bayer/AppLib/TableDef.vb
@@ -2179,7 +2179,6 @@
             Public TABLE_NAME As String
             Public STATUS As String
             Public NOTE As String
-            Public EXPORTIMPORT As String
         End Structure
         Public Class Column
             Public Const INPUT_DATE As String = "INPUT_DATE"
