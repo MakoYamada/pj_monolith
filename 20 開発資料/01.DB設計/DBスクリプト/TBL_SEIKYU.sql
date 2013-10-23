9883b84541a2ecf97e60a7584a3a330c5d50ef01
Kijitani <kijitani@localhost>
2013-10-23 01:59:38 +0000

    Vss2Git

--- 20 開発資料/01.DB設計/DBスクリプト/TBL_SEIKYU.sql
+++ 20 開発資料/01.DB設計/DBスクリプト/TBL_SEIKYU.sql
@@ -9,13 +9,14 @@ SET ANSI_PADDING ON
 GO
 CREATE TABLE [dbo].[TBL_SEIKYU](
 	[KOUENKAI_NO] [nvarchar](14) COLLATE Japanese_CI_AS  NOT NULL,
-	[SEISAN_YM] [nvarchar](8) COLLATE Japanese_CI_AS  NOT NULL,
+	[SHIHARAI_NO] [nvarchar](14) COLLATE Japanese_CI_AS  NULL,
+	[SEISAN_YM] [nvarchar](6) COLLATE Japanese_CI_AS  NULL,
 	[SHOUNIN_KUBUN] [nvarchar](1) COLLATE Japanese_CI_AS  NULL,
 	[SHOUNIN_DATE] [nvarchar](8) COLLATE Japanese_CI_AS  NULL,
 	[KAIJOHI_TF] [nvarchar](10) COLLATE Japanese_CI_AS  NULL,
 	[KIZAIHI_TF] [nvarchar](10) COLLATE Japanese_CI_AS  NULL,
 	[INSHOKUHI_TF] [nvarchar](10) COLLATE Japanese_CI_AS  NULL,
-	[KEI_991330401_1] [nvarchar](10) COLLATE Japanese_CI_AS  NULL,
+	[KEI_991330401_TF] [nvarchar](10) COLLATE Japanese_CI_AS  NULL,
 	[HOTELHI_TF] [nvarchar](10) COLLATE Japanese_CI_AS  NULL,
 	[JR_TF] [nvarchar](10) COLLATE Japanese_CI_AS  NULL,
 	[AIR_TF] [nvarchar](10) COLLATE Japanese_CI_AS  NULL,
@@ -32,17 +33,18 @@ CREATE TABLE [dbo].[TBL_SEIKYU](
 	[KAIJOUHI_T] [nvarchar](10) COLLATE Japanese_CI_AS  NULL,
 	[KIZAIHI_T] [nvarchar](10) COLLATE Japanese_CI_AS  NULL,
 	[INSHOKUHI_T] [nvarchar](10) COLLATE Japanese_CI_AS  NULL,
-	[KEI_991330401_2] [nvarchar](10) COLLATE Japanese_CI_AS  NULL,
+	[KEI_991330401_T] [nvarchar](10) COLLATE Japanese_CI_AS  NULL,
 	[JINKENHI_T] [nvarchar](10) COLLATE Japanese_CI_AS  NULL,
 	[OTHER_T] [nvarchar](10) COLLATE Japanese_CI_AS  NULL,
 	[KANRIHI_T] [nvarchar](10) COLLATE Japanese_CI_AS  NULL,
 	[KEI_41120200_T] [nvarchar](10) COLLATE Japanese_CI_AS  NULL,
 	[KEI_T] [nvarchar](10) COLLATE Japanese_CI_AS  NULL,
-	[SEIKYU_NO_TOPTOUR] [nvarchar](14) COLLATE Japanese_CI_AS  NULL,
+	[SEIKYU_NO_TOPTOUR] [nvarchar](14) COLLATE Japanese_CI_AS  NOT NULL,
 	[TAXI_T] [nvarchar](10) COLLATE Japanese_CI_AS  NULL,
 	[TAXI_SEISAN_T] [nvarchar](10) COLLATE Japanese_CI_AS  NULL,
 	[SEISANSHO_URL] [nvarchar](255) COLLATE Japanese_CI_AS  NULL,
 	[TAXI_TICKET_URL] [nvarchar](255) COLLATE Japanese_CI_AS  NULL,
+	[SEISAN_KANRYO] [nvarchar](510) COLLATE Japanese_CI_AS  NULL,
 	[MR_JR] [nvarchar](12) COLLATE Japanese_CI_AS  NULL,
 	[MR_HOTEL] [nvarchar](12) COLLATE Japanese_CI_AS  NULL,
 	[SEND_FLAG] [nvarchar](1) COLLATE Japanese_CI_AS  NULL,
@@ -54,7 +56,7 @@ CREATE TABLE [dbo].[TBL_SEIKYU](
  CONSTRAINT [PK_TBL_SEIKYU] PRIMARY KEY CLUSTERED 
 (
 	[KOUENKAI_NO] ASC,
-	[SEISAN_YM] ASC
+	[SEIKYU_NO_TOPTOUR] ASC
 ) ON [PRIMARY]
 ) ON [PRIMARY]
 
