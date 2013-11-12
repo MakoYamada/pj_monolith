c14fcba72b9246d856ff1a2ad06d2e9e82e50631
Kijitani <kijitani@localhost>
2013-11-12 10:38:18 +0000

    Vss2Git

--- 20 開発資料/01.DB設計/DBスクリプト/MS_ZEI.sql
+++ 20 開発資料/01.DB設計/DBスクリプト/MS_ZEI.sql
@@ -14,10 +14,10 @@ CREATE TABLE [dbo].[MS_ZEI](
 	[START_DATE] [nvarchar](8) COLLATE Japanese_CI_AS  NULL,
 	[END_DATE] [nvarchar](8) COLLATE Japanese_CI_AS  NULL,
 	[STOP_FLG] [nvarchar](1) COLLATE Japanese_CI_AS  NULL,
-	[INPUT_DATE] [nvarchar](20) COLLATE Japanese_CI_AS  NULL,
-	[INPUT_USER] [nvarchar](10) COLLATE Japanese_CI_AS  NULL,
-	[UPDATE_DATE] [nvarchar](20) COLLATE Japanese_CI_AS  NULL,
-	[UPDATE_USER] [nvarchar](10) COLLATE Japanese_CI_AS  NULL,
+	[INPUT_DATE] [nvarchar](14) COLLATE Japanese_CI_AS  NULL,
+	[INPUT_USER] [nvarchar](20) COLLATE Japanese_CI_AS  NULL,
+	[UPDATE_DATE] [nvarchar](14) COLLATE Japanese_CI_AS  NULL,
+	[UPDATE_USER] [nvarchar](20) COLLATE Japanese_CI_AS  NULL,
  CONSTRAINT [PK_MS_ZEI] PRIMARY KEY CLUSTERED 
 (
 	[ZEI_CD] ASC
