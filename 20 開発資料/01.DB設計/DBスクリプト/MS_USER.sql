5f56f9f129f089abab80b10fb3872b6d3f4a59e5
Kijitani <kijitani@localhost>
2013-11-12 10:33:19 +0000

    Vss2Git

--- 20 開発資料/01.DB設計/DBスクリプト/MS_USER.sql
+++ 20 開発資料/01.DB設計/DBスクリプト/MS_USER.sql
@@ -15,10 +15,10 @@ CREATE TABLE [dbo].[MS_USER](
 	[KENGEN_SEISAN] [nvarchar](1) COLLATE Japanese_CI_AS NULL,
 	[USER_NAME] [nvarchar](100) COLLATE Japanese_CI_AS NULL,
 	[STOP_FLG] [nvarchar](1) COLLATE Japanese_CI_AS NULL,
-	[INPUT_DATE] [nvarchar](20) COLLATE Japanese_CI_AS NULL,
-	[INPUT_USER] [nvarchar](10) COLLATE Japanese_CI_AS NULL,
-	[UPDATE_DATE] [nvarchar](20) COLLATE Japanese_CI_AS NULL,
-	[UPDATE_USER] [nvarchar](10) COLLATE Japanese_CI_AS NULL,
+	[INPUT_DATE] [nvarchar](14) COLLATE Japanese_CI_AS NULL,
+	[INPUT_USER] [nvarchar](20) COLLATE Japanese_CI_AS NULL,
+	[UPDATE_DATE] [nvarchar](14) COLLATE Japanese_CI_AS NULL,
+	[UPDATE_USER] [nvarchar](20) COLLATE Japanese_CI_AS NULL,
  CONSTRAINT [PK_MS_USER] PRIMARY KEY CLUSTERED 
 (
 	[LOGIN_ID] ASC
