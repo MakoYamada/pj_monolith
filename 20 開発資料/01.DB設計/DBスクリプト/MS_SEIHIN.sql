b4bee41fbe09d41696cf93910fa4bda17795b504
Kijitani <kijitani@localhost>
2013-11-12 10:34:33 +0000

    Vss2Git

--- 20 開発資料/01.DB設計/DBスクリプト/MS_SEIHIN.sql
+++ 20 開発資料/01.DB設計/DBスクリプト/MS_SEIHIN.sql
@@ -11,10 +11,10 @@ CREATE TABLE [dbo].[MS_SEIHIN](
 	[SYSTEM_ID] [nvarchar](10) COLLATE Japanese_CI_AS NULL,
 	[SEIHIN] [nvarchar](80) COLLATE Japanese_CI_AS NOT NULL,
 	[STOP_FLG] [nvarchar](1) COLLATE Japanese_CI_AS NULL,
-	[INPUT_DATE] [nvarchar](20) COLLATE Japanese_CI_AS NULL,
-	[INPUT_USER] [nvarchar](10) COLLATE Japanese_CI_AS NULL,
-	[UPDATE_DATE] [nvarchar](20) COLLATE Japanese_CI_AS NULL,
-	[UPDATE_USER] [nvarchar](10) COLLATE Japanese_CI_AS NULL,
+	[INPUT_DATE] [nvarchar](14) COLLATE Japanese_CI_AS NULL,
+	[INPUT_USER] [nvarchar](20) COLLATE Japanese_CI_AS NULL,
+	[UPDATE_DATE] [nvarchar](14) COLLATE Japanese_CI_AS NULL,
+	[UPDATE_USER] [nvarchar](20) COLLATE Japanese_CI_AS NULL,
  CONSTRAINT [PK_MS_SEIHIN] PRIMARY KEY CLUSTERED 
 (
 	[SEIHIN] ASC
