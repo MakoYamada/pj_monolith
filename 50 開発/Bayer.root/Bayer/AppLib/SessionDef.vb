b37e62ae2409a32bbec9d08b1ff1e967c37f005b
Yamada <yamada@localhost>
2013-11-13 08:14:55 +0000

    Vss2Git

--- 50 開発/Bayer.root/Bayer/AppLib/SessionDef.vb
+++ 50 開発/Bayer.root/Bayer/AppLib/SessionDef.vb
@@ -33,6 +33,7 @@
     Public Const KouenkaiRireki_TBL_KOUENKAI As String = "KouenkaiRireki_TBL_KOUENKAI"
     Public Const KouenkaiRireki_SEQ As String = "KouenkaiRireki_SEQ"
     Public Const KouenkaiRireki_PageIndex As String = "KouenkaiRireki_PageIndex"
+    Public Const KouenkaiRirekiPrint_SQL As String = "KouenkaiRirekiPrint_SQL"
     Public Const KouenkaiPrint_SQL As String = "KouenkaiPrint_SQL"
     Public Const NewKouenkaiPrint_SQL As String = "NewKouenkaiPrint_SQL"
     Public Const KaijoRireki As String = "KaijoRireki"
