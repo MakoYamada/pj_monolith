029308d3ec8dfbf649b97bf6aa0769c591fc2789
Kijitani <kijitani@localhost>
2013-11-26 04:47:23 +0000

    Vss2Git

---
+++ 50 開発/Bayer.root/Bayer/Bayer/SapCsv.aspx.vb
@@ -0,0 +1,10 @@
+﻿Imports CommonLib
+Imports AppLib
+Partial Public Class SapCsv
+    Inherits WebBase
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+End Class
