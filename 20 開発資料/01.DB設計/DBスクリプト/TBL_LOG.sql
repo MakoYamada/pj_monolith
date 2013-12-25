cd7b71923ae12e59b0d425af302a687cdd417820
Tanaka <tanaka@localhost>
2013-12-25 06:00:24 +0000

    Vss2Git

--- 20 開発資料/01.DB設計/DBスクリプト/TBL_LOG.sql
+++ 20 開発資料/01.DB設計/DBスクリプト/TBL_LOG.sql
@@ -8,13 +8,18 @@ GO
 SET ANSI_PADDING ON
 GO
 CREATE TABLE [dbo].[TBL_LOG](
+	[LOG_NO] [int] IDENTITY(1,1),
 	[INPUT_DATE] [nvarchar](14) COLLATE Japanese_CI_AS  NULL,
 	[INPUT_USER] [nvarchar](20) COLLATE Japanese_CI_AS  NULL,
 	[SYORI_KBN] [nvarchar](1) COLLATE Japanese_CI_AS  NULL,
 	[SYORI_NAME] [nvarchar](50) COLLATE Japanese_CI_AS  NULL,
 	[TABLE_NAME] [nvarchar](50) COLLATE Japanese_CI_AS  NULL,
 	[STATUS] [nvarchar](1) COLLATE Japanese_CI_AS  NULL,
-	[NOTE] [nvarchar](1000) COLLATE Japanese_CI_AS  NULL,
+	[NOTE] [nvarchar](3000) COLLATE Japanese_CI_AS  NULL,
+ CONSTRAINT [PK_TBL_LOG] PRIMARY KEY CLUSTERED 
+(
+	[LOG_NO] ASC
+) ON [PRIMARY]
 ) ON [PRIMARY]
 
 GO
