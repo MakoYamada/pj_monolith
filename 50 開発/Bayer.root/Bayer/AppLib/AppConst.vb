e34771d018df4f1a6878b3e247f4b90abe9e5541
Yamada <yamada@localhost>
2014-02-10 05:53:29 +0000

    Vss2Git

--- 50 開発/Bayer.root/Bayer/AppLib/AppConst.vb
+++ 50 開発/Bayer.root/Bayer/AppLib/AppConst.vb
@@ -34,7 +34,7 @@ Public Class AppConst
     End Class
 
     Public Class KOTSUHOTEL
-        Public Const NotSelect As String = "99"
+        Public Const NotSelect As String = ""
 
         Public Class RECORD_KUBUN
             'ƒf[ƒ^‹æ•ª
