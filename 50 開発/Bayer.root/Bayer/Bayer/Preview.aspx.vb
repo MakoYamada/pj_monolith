73df745b4a9c4010dba152dd2f83cc814266e3f1
Yamada <yamada@localhost>
2014-02-20 04:38:30 +0000

    Vss2Git

--- 50 開発/Bayer.root/Bayer/Bayer/Preview.aspx.vb
+++ 50 開発/Bayer.root/Bayer/Bayer/Preview.aspx.vb
@@ -239,9 +239,9 @@ Partial Public Class Preview
         rpt1.PageSettings.Orientation = DataDynamics.ActiveReports.Document.PageOrientation.Portrait
 
         '必要に応じマージン設定
-        rpt1.PageSettings.Margins.Top = ActiveReport.CmToInch(0.9)
-        rpt1.PageSettings.Margins.Bottom = ActiveReport.CmToInch(0.9)
-        rpt1.PageSettings.Margins.Left = ActiveReport.CmToInch(0.9)
+        rpt1.PageSettings.Margins.Top = ActiveReport.CmToInch(1.5)
+        rpt1.PageSettings.Margins.Bottom = ActiveReport.CmToInch(1.5)
+        rpt1.PageSettings.Margins.Left = ActiveReport.CmToInch(1.5)
         rpt1.PageSettings.Margins.Right = ActiveReport.CmToInch(0.9)
 
         rpt1.KOTSUHOTEL_DATA = RPT_KOTSUHOTEL
