4689b1b8f7ae1d12e1c5fc5c86308c90911dd60f
Myamada <myamada@localhost>
2017-06-05 01:12:18 +0000

    Vss2Git

---
+++ 20 開発資料/01.DB設計/DBスクリプト/過去データ削除SQL(≦20151231)/del_shounin.sql
@@ -0,0 +1,20 @@
+select count(*) from tbl_kouenkai
+where kouenkai_no in 
+(SELECT
+    TBL_KOUENKAI.kouenkai_no
+FROM
+    TBL_KOUENKAI INNER JOIN (SELECT
+                                KOUENKAI_NO,
+                                MAX(TIME_STAMP) TIME_STAMP_MAX
+                            FROM
+                                TBL_KOUENKAI
+                            WHERE
+                                FROM_DATE <= '20151231' AND
+                                FROM_DATE <> ''
+                            GROUP BY
+                                KOUENKAI_NO
+                            ) WK1 ON TBL_KOUENKAI.KOUENKAI_NO=WK1.KOUENKAI_NO AND TBL_KOUENKAI.TIME_STAMP=WK1.TIME_STAMP_MAX
+WHERE
+    TBL_KOUENKAI.FROM_DATE <= '20151231' AND
+    TBL_KOUENKAI.FROM_DATE <> ''
+)
